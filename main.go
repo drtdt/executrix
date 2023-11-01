9068ede154291baf20ee149481aadbaa7275920e
Dominique Toepfer <dominique@toepfer-web.de>
2023-11-01 17:41:55 +0100

    setup basic http server

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
+
 func main() {
-	slog.Info("Hello, world!")
+	const port uint16 = 8080
+
+	http.HandleFunc("/", handler)
+	slog.Info(fmt.Sprintf("Start listening on port %d", port))
+	slog.Error(http.ListenAndServe(fmt.Sprintf("localhost:%d", port), nil).Error())
 }
