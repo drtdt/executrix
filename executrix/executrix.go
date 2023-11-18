b19807e7dd022e05e2dc6b39f3ac0e6b89d3d818
Dominique Toepfer <dominique@toepfer-web.de>
2023-11-18 18:52:10 +0100

    Refactor execution function and add stop and new buttons (not working yet)

--- executrix/executrix.go
+++ executrix/executrix.go
@@ -10,25 +10,37 @@ func ExecutePipeline(p *data.Pipeline, stepInfo []data.StepInfo) {
 	slog.Info("Starting pipeline")
 
 	for _, step := range stepInfo {
-		if step.Checked {
-			pStep := p.FindStep(step.StepName)
-			if pStep == nil {
-				slog.Error("Could not find Pipeline Step!")
-				// todo error handling
-			}
-
-			slog.Info("Excuting step", "step", step.StepName)
-			pStep.SetState(data.Running)
-
-			// dummy implementation
-			time.Sleep(10000 * time.Millisecond)
-
-			pStep.SetState(data.Success)
-		} else {
+		if !step.Checked {
 			slog.Info("Skipping unchecked step", "step", step.StepName)
+			continue
+		}
+
+		pStep := p.FindStep(step.StepName)
+		if pStep == nil {
+			slog.Error("Could not find Pipeline Step!")
+			// todo error handling
+		}
+
+		pStep.SetState(data.Running)
+
+		switch s := pStep.(type) {
+		case *data.PSStep:
+			execPS(s)
+		default:
+			slog.Error("Could not find Pipeline Step!")
+			pStep.SetState(data.Failed)
+			continue
 		}
 	}
 
 	p.IsRunning = false
 	slog.Info("Pipeline finished")
+}
+
+func execPS(step *data.PSStep) {
+	slog.Info("Excuting step", "step", step.Name)
+	// dummy implementation
+	time.Sleep(10000 * time.Millisecond)
+
+	step.SetState(data.Success)
 }
